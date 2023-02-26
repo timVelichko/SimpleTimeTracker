b5ff9e43b5c76eafd4e65661ff0a415e2d84535f
Tim Vialichka <timvelichko@gmail.com>
2023-02-26 14:54:50 +0100

    Initial Commit

---
+++ SimpleTimeTracker/SimpleTimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTimeTrackerApp.swift
+//  SimpleTimeTracker
+//
+//  Created by Tim Vialichka on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
